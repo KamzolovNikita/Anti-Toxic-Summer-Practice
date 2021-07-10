f9799d9fdf4403a946ad00c00b584d4c0c7de0e5
FollowJust <gladkikha@outlook.com>
2021-07-10 17:25:19 +0300

    +tests +paths for vertexes

--- BellmanFord/app/src/main/java/com/example/android/bellmanford/algorithm/Graph.kt
+++ BellmanFord/app/src/main/java/com/example/android/bellmanford/algorithm/Graph.kt
@@ -3,11 +3,11 @@ package com.example.android.bellmanford.algorithm
 typealias Neighbour = Pair<String, Int>
 typealias Neighbours = MutableList<Neighbour>
 
+
 class Graph(val adjacencyMap: Map<String, Neighbours>) {
 
     // An inner class to represent a weighted edge in graph
     inner class Edge(var src: String, var dest: String, var weight: Double)
-
 
     var vertexAmount = adjacencyMap.size
     var edgeAmount = 0
@@ -15,11 +15,5 @@ class Graph(val adjacencyMap: Map<String, Neighbours>) {
         adjacencyMap.forEach {
             edgeAmount += it.value.size
         }
-    }
-
-    // A utility function used to print the solution
-    fun printArr(dist: IntArray, V: Int) {
-        println("Vertex Distance from Source")
-        for (i in 0 until V) println(i.toString() + "\t\t" + dist[i])
     }
 }
